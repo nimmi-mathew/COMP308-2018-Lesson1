15ce42b1372d1c200b1a0f1eb32eb0f1e0a0d60b
Nimmi Mathew <nmathew7@my.centennialcollege.ca>
2018-01-23 19:55:17 -0500

    Added Express

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-const http = require('http'); //
+// const http = require('http'); // include a piece of functionality , http module
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// const hostname = '127.0.0.1';
+// const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World\n');
+// });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+var express = require('express')
+var app = express()
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.listen(3000, () => console.log("App listening on port 3000"));
